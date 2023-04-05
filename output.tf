a0b3e85cb9e5ed8e8078351a2f20a81c2e0d604e
Sravani <sravani11.ec@gmail.com>
2023-04-05 10:51:35 +0530

    updated

--- output.tf
+++ output.tf
@@ -3,6 +3,7 @@ output "demo_ec2_id" {
   value       = aws_instance.demo_ec2.id
 }
 
+
 output "demo_ec2_private_ip" {
   description = "Print the Private IP of EC2 Instance"
   value       = aws_instance.demo_ec2.private_ip
